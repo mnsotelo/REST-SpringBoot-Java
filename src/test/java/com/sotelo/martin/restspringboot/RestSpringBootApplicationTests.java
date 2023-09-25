c831a73de4a8e8d52d8d293082d090272403a304
Martin Sotelo <martinsoteloferry.inf3@gmail.com>
2023-09-25 22:45:01 +0200

    Add gitignore

---
+++ src/test/java/com/sotelo/martin/restspringboot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sotelo.martin.restspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
