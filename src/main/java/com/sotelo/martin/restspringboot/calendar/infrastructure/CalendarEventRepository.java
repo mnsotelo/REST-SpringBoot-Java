05ddb01fedf4cc42059200f4c52276a9a37882d5
Martin Sotelo <martinsoteloferry.inf3@gmail.com>
2023-09-27 15:28:43 +0200

    Change scope of Repository.

--- src/main/java/com/sotelo/martin/restspringboot/calendar/infrastructure/CalendarEventRepository.java
+++ src/main/java/com/sotelo/martin/restspringboot/calendar/infrastructure/CalendarEventRepository.java
@@ -2,5 +2,5 @@ package com.sotelo.martin.restspringboot.calendar.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CalendarEventRepository extends JpaRepository<CalendarEvent, Long> {
+interface CalendarEventRepository extends JpaRepository<CalendarEvent, Long> {
 }
